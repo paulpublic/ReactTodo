84ed18c45b6f8dfe52003d9d0106535127cf3359
Paul Jansen <paul@jnsnp.net>
2016-06-27 20:49:02 +0200

    Added redux store

--- app/app.jsx
+++ app/app.jsx
@@ -4,6 +4,17 @@ var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
 var TodoApp = require('TodoApp');
 
+var actions = require('actions');
+var store = require('configureStore').configure();
+
+store.subscribe(() => {
+  console.log('New State', store.getState());
+});
+
+store.dispatch(actions.addTodo("Buy milk"));
+store.dispatch(actions.setSearchText('yard'));
+store.dispatch(actions.toggleShowCompleted());
+
 // Load foundation
 $(document).foundation();
 
