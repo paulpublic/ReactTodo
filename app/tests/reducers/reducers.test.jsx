1936d5e2a1ab0ba231c3cc29e43f6fb251370821
Paul Jansen <paul@jnsnp.net>
2016-06-28 16:49:08 +0200

    Update todos on firebase

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -47,12 +47,7 @@ describe('Reducers', () => {
       expect(res[0]).toEqual(action.todo);
     });
 
-    it('should toggle todo', () => {
-      var action = {
-        type: 'TOGGLE_TODO',
-        id: 11
-      };
-
+    it('should update todo', () => {
       var todos = [{
         id: 11,
         text: 'Test 11',
@@ -61,10 +56,22 @@ describe('Reducers', () => {
         completedAt: 125
       }];
 
+      var updates = {
+        completed: false,
+        completedAt: null
+      };
+
+      var action = {
+        type: 'UPDATE_TODO',
+        id: todos[0].id,
+        updates
+      };
+
       var res = reducers.todosReducer(df(todos), df(action));
 
-      expect(res[0].completed).toBe(false);
-      expect(res[0].completedAt).toBe(undefined);
+      expect(res[0].completed).toEqual(updates.completed);
+      expect(res[0].completedAt).toEqual(updates.completedAt);
+      expect(res[0].text).toEqual(todos[0].text);
     });
 
     it('should add existing todos', () => {
